17d70aebacec99d247f593ff925ec1678fd309e3
usmonovahanifa <usmonovahanifa1@gmail.com>
2024-08-02 13:13:38 +0500

    USER ADDED

--- src/models/model.sql
+++ src/models/model.sql
@@ -14,3 +14,8 @@ INSERT INTO
     users (full_name, age, job)
 VALUES
     ('Usmonova Hanifa', 15, null);
+
+INSERT INTO
+    users (full_name, age, job)
+VALUES
+    ('Hanifa Usmonova', 20, 'Student');
