37c27de4b00a043878c650de808f775f60b472c6
usmonovahanifa <usmonovahanifa1@gmail.com>
2024-08-02 14:18:27 +0500

    route for get user by id function added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { getUsers, createUser, updateUsers, deleteUsers } = require('../controllers/usersController');
+const { getUsers, createUser, updateUsers, deleteUsers, getUserById } = require('../controllers/usersController');
 const router = Router()
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
 router.post('/users', createUser);
 router.patch("/users/:id", updateUsers);
 router.delete('/users/:id', deleteUsers);
